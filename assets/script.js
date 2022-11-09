2f9cac84cd76b965e6e75b44208d4c1aab775758
Jacob Van Dyke <jacobvandyke194@gmail.com>
2022-11-08 22:30:52 -0700

    finised linking buttons to corresponding inputs, need to add to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,26 @@
-let currentDay = document.getElementById('currentDay');
-let currentTime = moment().format("ddd. MMMM Do YYYY, h:mm a")
-currentDay.innerHTML = currentTime;
+//displays current time and date at top of page and refreshes every second
+function displayTime() {
+    let currentDay = document.getElementById('currentDay');
+    let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+    currentDay.innerHTML = currentTime;
+}
+setInterval(displayTime, 1000);
+
+// grabbing DOM elements
+
+var saveBtn = $('.saveBtn');
+
+
+saveBtn.on('click', test) 
+
+
+function test(){
+    var rowParent = $(this).parents()[1]
+    var rowChild = $(rowParent).children()[0]
+    var input = $(rowChild).children()[0]
+    console.log($(input).val())
+};
+
+
+
+
